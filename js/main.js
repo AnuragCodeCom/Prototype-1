ba81f1fc2dd603f5bbf907648eebcbe51dd5ade1
AnuragCodeCom <anuragsharmaviiitulip@gmail.com>
2023-07-30 20:53:26 +0530

    Bug Fix

--- js/main.js
+++ js/main.js
@@ -1,26 +1,13 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 var loader = document.getElementById("preload")
-window.addEventListener("load" , function(){
+window.addEventListener("load" , async function(){
+    await sleep(3000);
     loader.style.background = "none"
     loader.addEventListener("transitionend" , function(){
         document.body.removeChild(loader)
     })
 })
-
-var reveals = document.querySelectorAll('.reveal')
-
-window.addEventListener('scroll'  , reveal);
-
-function reveal(){
-    for(var i = 0; i < reveals.length; i++){
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
-        if (revealtop < windowheight - revealpoint){
-            reveals[i].classlist.add('active')
-        }
-        else{
-            reveals[i].classlist.remove('active')
-        }
-    }
-
-}
